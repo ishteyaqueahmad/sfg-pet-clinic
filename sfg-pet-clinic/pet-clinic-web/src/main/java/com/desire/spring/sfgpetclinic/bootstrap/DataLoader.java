b4391767e67ce747989c131b279134d422c67e6e
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-21 12:13:01 +0530

    resolved issue with file structure

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/desire/spring/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,10 @@
+package com.desire.spring.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DataLoader implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
